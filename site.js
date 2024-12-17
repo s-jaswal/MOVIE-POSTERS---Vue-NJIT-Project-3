f62eee9c0955a713c24371c9838e2b6191c94c40
s-jaswal <525SJASWAL@FRHSD.COM>
2024-12-17 11:40:57 -0500

    Step 3 - complete

--- site.js
+++ site.js
@@ -24,19 +24,23 @@
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+            return {
+                  // This holds your movies.json data.
+                  movies: []
+                  title: "IMDB: Saanvi's Top 8 Movies",
+                  owner: "Saanvi Jaswal",
+                  github: 'https://github.com/s-jaswal/MOVIE-POSTERS---Vue-NJIT-Project-3'
+
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+
+            }
+      },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
       }
